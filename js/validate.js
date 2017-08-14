7123d511acd94594c8a34ae4783ae8527350809c
Kasia Grabowska <kattie.grab@gmail.com>
2017-08-14 19:44:11 +0200

    validation

--- js/validate.js
+++ js/validate.js
@@ -3,31 +3,55 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const button = document.querySelector('#submit-button');
         const input = document.querySelector('#input-password');
+        const alert = document.querySelector('.alert');
+        const errorTriangle = document.querySelector('.error-triangle');
 
         button.addEventListener("click", function(event) {
             console.log(input.value);
 
-            var alert = document.querySelector('#password-alert');
 
-            if (input.value.length < 8) {
-        		alert.innerHTML = "Password is too short. It has to be at least 8 characters long!";
-        		input.className += ' error';
-	        }
+         //    if (input.value.length < 8) {
+        	// 	alert.innerHTML = "Password is too short. It has to be at least 8 characters long!";
+        	// 	input.className += ' error';
+	        // }
 	        if (input.value === '') {
 	        	alert.innerHTML = "Please enter your password";
 	        	input.className += ' error';
+    			errorTriangle.style.display = "block";
 	        } else {
 	        	console.log(window.location);
-	        	window.location.href = 'index.html';
+	        	// window.location.href = 'index.html';
+	        	communicateApi(input.value);
 	        }
         });
+        
+       function communicateApi(password) {
+        $.ajax({
+		  type: "post",
+		  data: {
+		    login: "efi",
+		    password: password
+		  },
+		  url: "https://efigence-camp.herokuapp.com/api/login",
+		  error: function(response) {
+		    console.log(response.responseText);
 
+		    const apiResponse = JSON.parse(response.responseText);
 
+		    input.className += ' error';
+		    alert.innerHTML = apiResponse.message;
+		    errorTriangle.style.display = "block";
 
+		  },
+		  success: function(response) {
+		    console.log(response);
+		    input.className -= ' error'
+		    errorTriangle.style.display = "none";
+		    window.location.href = 'index.html';
+		  }
+		});
+      };
+})();
+	
+});
 
-    })();
-})
-
-//pobrac wartosc hasla
-//walidacja: zczytanie pola hasla, przygotowanie instrukcji war ktora sprawdz czy pola sa wypelnione, jezeli ktores nie jest
-//dodaj np klase error(obramowanie na czerwono), jezeli wszystkie pola wprowadzone to zmien lokalizacje na index.html
