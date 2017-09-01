ded050ef0a51f3f1493d5a3a3c37e35143fb4d9d
Kasia Grabowska <kattie.grab@gmail.com>
2017-09-01 22:50:23 +0200

    added some more transfer page styles

--- js/payment.js
+++ js/payment.js
@@ -7,6 +7,19 @@ $.get("https://efigence-camp.herokuapp.com/api/data/summary", data => {
 
 	accountAmount.innerText = balanceValue + ' PLN';
 });
+// basic form validation - Are all fields filled?
+const continueBtn = document.querySelector('.btn-continue');
+const inputRecipient = document.querySelector('.recipient-input');
+const recipientErrorContainer = document.querySelector('.recipient-error-msg')
+
+continueBtn.addEventListener('click', () => {
+	if (inputRecipient.value === '') {
+		recipientErrorContainer.innerText = 'Please enter recipient name!';
+		inputRecipient.className += ' error';
+	}
+
+
+})
 // ========== end of account balance =======
 // date picker
   $( function() {
